a23063888b4c95e360f25f2c8faba285b0fdf783
tamerakkuzu09-tefo <tamerakkuzu09@gmail.com>
2026-03-14 20:11:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,18 @@ import os
 
 PORT = int(os.environ.get("PORT", 8080))
 
-HTML = open("index.html", encoding="utf-8").read()
+with open("index.html", encoding="utf-8") as f:
+    HTML = f.read()
+
+HTML_BYTES = HTML.encode("utf-8")
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html; charset=utf-8")
+        self.send_header("Content-Length", str(len(HTML_BYTES)))
         self.end_headers()
-        self.wfile.write(HTML.encode("utf-8"))
+        self.wfile.write(HTML_BYTES)
     def log_message(self, format, *args):
         pass
 
